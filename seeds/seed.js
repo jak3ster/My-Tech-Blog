c9f7e228dad6438741887c6756a0f72ddb894969
Jay A <jautus@hotmail.com>
2022-07-04 21:52:59 -0700

    Added models, js, css, seeds, helpers, and handlebars

---
+++ seeds/seed.js
@@ -0,0 +1,42 @@
+// const seedUsers = require('./user-seeds');
+// const seedPosts = require('./post-seeds');
+// const seedComments = require('./comment-seeds');
+
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Post.bulkCreate(postData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Comment.bulkCreate(commentData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+
+// const seedDatabase = async() => {
+//     await sequelize.sync({ force: true });
+//     await seedUsers();
+//     await seedPosts();
+//     await seedComments();
+//     process.exit(0);
+// };
+
+seedDatabase();
