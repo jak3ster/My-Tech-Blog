f1967b1f1bd316400f59be821382169cf60d4861
jak3ster <jautus@hotmail.com>
2022-07-14 00:36:26 -1000

    Restore files from local backup

---
+++ public/js/addPost.js
@@ -0,0 +1,26 @@
+// this function corresponds to the add-post form
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('textarea[name="content"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+document.querySelector('#add-post-form').addEventListener('submit', newFormHandler);
